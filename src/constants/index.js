2cba33092426f1ee305504d4c7f222f74e8a709b
Łukasz <103317892+luksi02@users.noreply.github.com>
2024-01-30 23:41:46 +0100

    Add files via upload

--- src/constants/index.js
+++ src/constants/index.js
@@ -62,16 +62,16 @@ export const skills = [
         name: "MongoDB",
         type: "Database",
     },
-    {
-        imageUrl: motion,
-        name: "Motion",
-        type: "Animation",
-    },
-    {
-        imageUrl: mui,
-        name: "Material-UI",
-        type: "Frontend",
-    },
+    // {
+    //     imageUrl: motion,
+    //     name: "Motion",
+    //     type: "Animation",
+    // },
+    // {
+    //     imageUrl: mui,
+    //     name: "Material-UI",
+    //     type: "Frontend",
+    // },
     {
         imageUrl: nextjs,
         name: "Next.js",
@@ -119,7 +119,7 @@ export const experiences = [
         points: [
             "TBD",
         ],
-    },    
+    },
 ];
 
 export const socialLinks = [
@@ -142,17 +142,115 @@ export const socialLinks = [
 
 export const projects = [
     {
-        iconUrl: threads,
-        theme: 'btn-back-green',
-        name: 'Full Stack Threads Clone',
-        description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-        link: 'https://github.com/adrianhajdin/threads',
+        iconUrl: pricewise,
+        theme: 'btn-back-orange',
+        name: '3D-Portfolio',
+        description: 'A website portfolio of completed projects, with basic 3D animations using the ThreeJS, ReactJS, and TailwindCSS libraries.',
+        link: 'https://vite-3d-island.vercel.app/',
+        // githubLink: 'https://github.com/luksi02/vite-3d-island'
     },
     {
         iconUrl: car,
         theme: 'btn-back-blue',
         name: 'Car Finding App',
         description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
-        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
+        link: 'https://luksi02-carhub.vercel.app/',
+        // githubLink: 'https://github.com/luksi02/luksi02_carhub'
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-green',
+        name: 'CS50',
+        description: 'Harvard University’s introduction to the intellectual enterprises of computer science and the art of programming.',
+        link: 'https://github.com/luksi02/CS50',
+        // githubLink: 'https://github.com/luksi02/CS50'
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-green',
+        name: 'MERN_Memories',
+        description: 'Meta’s Threads clone - an social media communicator. Uses React and Tailwind CSS for frontend, and Next.js, Node.js and MongoDB as backend. Also uses Clerk.js for user authentication. App in development.',
+        link: 'https://github.com/luksi02/mern_memories',
+        // githubLink: 'https://github.com/luksi02/mern_memories'
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Full Stack Threads Clone',
+        description: 'Meta’s Threads clone - an social media communicator. Uses React and Tailwind CSS for frontend, and Next.js, Node.js and MongoDB as backend. Also uses Clerk.js for user authentication. App in development.',
+        link: 'https://github.com/luksi02/luksi02_threads_clone',
+        // githubLink: 'https://github.com/luksi02/luksi02_threads_clone'
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-green',
+        name: 'MERN_Image_Generator',
+        description: 'An Midjourney/DALL-E AI image generator (using OpenAIs DALL-E API), written in React with Tailwind CSS (frontend) and Express.js, Node.js and MongoDB as backend.',
+        link: 'https://mern-image-generator.vercel.app/',
+        // githubLink: 'https://github.com/luksi02/MERN_Image_Generator'
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-green',
+        name: 'MOVIE_APP',
+        description: 'Internet film base created in React framework, that uses Redux Toolkit for app state managment, film database from OMBD (through API).',
+        link: 'https://github.com/luksi02/React-Redux-Toolkit_Movie_App',
+        // githubLink: 'https://github.com/luksi02/React-Redux-Toolkit_Movie_App'
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-green',
+        name: 'AGONY',
+        description: 'Simple, text based RPG game, built using Django framework and databses. Game is still in development, I inted to upgrade it with JavaScript elements (frontend) and AI-based text generator (OpenAI, as in-game narrator) and perhaps AI-based graphic generator (DALL-E)',
+        link: 'https://github.com/luksi02/AGONy',
+        // githubLink: 'https://github.com/luksi02/AGONy'
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-green',
+        name: 'FAKESHOP',
+        description: 'Web app for viewing and purchasing products. Created in React, uses Redux for apps state managment and uses Fake Store API as store database.',
+        link: 'https://github.com/luksi02/JS_React_Redux_Fakeshop',
+        // githubLink: 'https://github.com/luksi02/JS_React_Redux_Fakeshop'
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-green',
+        name: 'THE ROOM',
+        description: 'Web app for conference room reservation/managment (Django framework).',
+        link: 'https://github.com/luksi02/The_Room',
+        // githubLink: 'https://github.com/luksi02/The_Room'
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-green',
+        name: 'JEDZONKO - SCURMLAB',
+        description: 'Web app built in 5-person team while working on two-week long sprint developing app for creation of daily (weekly) nutrition plans, using Scrum methodology for progress managment.',
+        link: 'https://github.com/luksi02/Jedzonko---ScrumLab',
+        // githubLink: 'https://github.com/luksi02/Jedzonko---ScrumLab'
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-green',
+        name: 'ODDAM W DOBRE RĘCE',
+        description: 'Web app for easy reporting items for donations to charity organisations (Django framework).',
+        link: 'https://github.com/luksi02/oddam_w_dobre_rece',
+        // githubLink: 'https://github.com/luksi02/oddam_w_dobre_rece'
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-green',
+        name: 'REACT_REDUX_JAVASCRIPT',
+        description: 'Repository consisting of various, simple scripts written in React-Redux, React and pure JS.',
+        link: 'https://github.com/luksi02/React_Redux_JavaScript',
+        // githubLink: 'https://github.com/luksi02/React_Redux_JavaScript'
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-green',
+        name: 'DRF',
+        description: 'Web app to manage/add images using Django REST Framework.',
+        link: 'https://github.com/luksi02/DRF',
+        // githubLink: 'https://github.com/luksi02/DRF'
     },
 ];
