08acda34650cdfb8bf7e14cc836b394e95839374
Łukasz <103317892+luksi02@users.noreply.github.com>
2024-02-01 18:12:27 +0100

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -4,6 +4,5 @@ export { Plane } from "./Plane";
 export { Carrier } from "./Carrier";
 export { Fox } from "./Fox";
 export { Drone } from "./Drone";
-export { Toad } from "./Toad";
 export { Yellow_drone } from "./Yellow_drone";
 export { Mech_drone } from "./Mech_drone";
