359550bb405b933929a4fdd1a75687f1c2407c10
Łukasz <103317892+luksi02@users.noreply.github.com>
2024-02-01 19:43:21 +0100

    Update HomeInfo.jsx

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -3,6 +3,14 @@ import { Link } from "react-router-dom";
 import { arrow } from "../assets/icons";
 
 const HomeInfo = ({ currentStage }) => {
+  if (currentStage === 0)
+    return (
+      <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+        Spin the space carrier to find out hidden
+        <br />
+        messages! Be free to check it out!
+      </h1>
+    );
   if (currentStage === 1)
     return (
       <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
@@ -47,15 +55,15 @@ const HomeInfo = ({ currentStage }) => {
   if (currentStage === 4) {
     return (
       <div className='info-box'>
-      <p className='font-medium sm:text-xl text-center'>
-        Need a project done or looking for a dev? <br/> I'm just a few keystrokes away
-      </p>
+        <p className='font-medium sm:text-xl text-center'>
+          Need a project done or looking for a dev? <br /> I'm just a few keystrokes away
+        </p>
 
-      <Link to='/contact' className='neo-brutalism-white neo-btn'>
-        Let's talk
-        <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
-      </Link>
-    </div>
+        <Link to='/contact' className='neo-brutalism-white neo-btn'>
+          Let's talk
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
     );
   }
 
